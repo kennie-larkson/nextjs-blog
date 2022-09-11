2ac89cf0b3f1db936e2477241da325644f3ee8ae
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-09-11 08:34:54 +0100

    deleted posts folder from pages due to netlify deployment error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { Component } from "react";
-import "../styles/global.css"
-
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
