cb5c9a66bbed563c4b38c67720423e7c5163022a
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-09-11 09:07:02 +0100

    deleted the lib folder

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,11 @@
 import Layout from "../components/layout";
 import Body from "../components/body";
-import styles from "./about.module.css";
 
 const About = () => {
-
   return (
     <>
       <Layout about>
-        <Body>
-        About Page
-        </Body>
-       
+        <Body>About Page</Body>
       </Layout>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import Body from "../components/body";
-import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
