e6d18d0069466aea0fdabfefcef9778e12262ff7
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-01-28 19:58:42 +0100

    changed intro message

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,18 @@ export default function Home({}) {
         <section className="flex flex-col phone:container bg-gray-50 text-justify py-2 px-3 rounded-sm font-mono leading-loose">
           <div className="">
             <p className=" ">
-              Hello, I'm <strong>Kennie</strong>, a software engineer 🤓 and
+              Welcome to my personal website! My name is Kehinde Lawal, and I am
+              a full stack web developer with over 3 years of experience. I
+              specialize in using technologies such as JavaScript, Node.js,
+              React, Express, TypeScript, NoSQL, SQL, Docker, and GIT Version
+              Control to build dynamic, responsive, and efficient web
+              applications. Throughout my career, I have worked on a wide range
+              of projects, honing my skills in both front-end and back-end
+              development. I am passionate about creating user-friendly,
+              high-performance web experiences that help businesses and
+              organizations achieve their goals. Thank you for visiting my
+              website, and I look forward to connecting with you.
+              {/* Hello, I'm <strong>Kennie</strong>, a software engineer 🤓 and
               this is my profile page. You can hang around to learn more about
               me.
               <br />
@@ -24,7 +35,7 @@ export default function Home({}) {
                 href="https://twitter.com/kennie_larkson"
               >
                 Twitter
-              </a>{" "}
+              </a>{" "} */}
             </p>
             <p className=" ">
               This is my portfolio website - here I will make a showroom of my
